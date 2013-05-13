93bff788a80e0993ddf9d9b76302277e0649ebe7
Jared Rosen <jaredarosen@gmail.com>
2013-05-12 20:44:20 -0400

    more abstraction

---
+++ App/qr.php
@@ -0,0 +1,13 @@
+		<div data-role="page" id="qr">
+			<div data-role="header" data-position="fixed">
+				<a href="#settings" data-role="button" data-icon="gear" data-prefetch>Settings</a>
+				<h1>Scan</h1>
+			</div>
+
+			<div data-role="content">
+				<img src="https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=jaredarosen%40gmail.com" alt="Smiley face" height="42" width="42"> 
+				<!--
+				Generate and display GR code
+				-->
+			</div>
+		</div>
---
+++ App/settings.php
@@ -0,0 +1,14 @@
+		<div data-role="page" id="settings">
+			<div data-role="header" data-position="fixed">
+				<a href="#qr" data-role="button" data-icon="back" data-prefetch>Back</a>
+				<h1>Settings</h1>
+			</div>
+
+			<div data-role="content">
+				<a data-role="button" data-icon="delete" data-prefetch>Logout</a>
+				<!--
+				Logout button
+				Map
+				-->
+			</div>
+		</div>

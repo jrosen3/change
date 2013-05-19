9a9329f208a13e9f82c35c82b7978fd7086a4024
Jared Rosen <jaredarosen@gmail.com>
2013-05-18 21:42:51 -0700

    paypal login etc

--- App/login.php
+++ App/login.php
@@ -1,15 +1,26 @@
 <?php include "header.php" ?>
 	<!-- body opened in header.php -->
+		<script src="https://www.paypalobjects.com/js/external/api.js"></script>
+		<script>
+			paypal.use( ["login"], function(login) {
+				login.render ({
+					"appid": "d3428641e41208c246d07b2e5f3cc7a5",
+					"authend": "sandbox",
+					"scopes": "profile email address https://uri.paypal.com/services/paypalattributes",
+					"containerid": "paypal",
+					"locale": "en-us",
+					"returnurl": "http://my.domain.here/return.php" //index.php
+				});
+			});
+		</script>
+
 		<div data-role="page" id="login">
 			<div data-role="header" data-position="fixed">
 				<h1>Login</h1>
 			</div>
 
 			<div data-role="content">
-				<!--
-				Login with PayPal api
-				Link to qr
-				-->
+				<div id="paypal"></div>
 			</div>
 		</div>
 	</body>
---
+++ hardware/server.php
@@ -0,0 +1,1 @@
+http://decode.kaywa.com/api/decodeCode?API-KEY=5348cb414c8c85d5760583fd6e00c952&code=2029998765

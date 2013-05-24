bbca96083156db38bb641c32172c0aa55568b9d3
Jared Rosen <jaredarosen@gmail.com>
2013-05-23 17:17:56 -0700

    link

--- hardware/header.php
+++ hardware/header.php
@@ -41,7 +41,7 @@
 
 		<script src="js/jquery.cycle.all.js"></script> <!--http://jquery.malsup.com/cycle/adv.html-->
 		<script src="js/jquery.easing.min.js"></script>
-		<script src="js/idle-timer.min.js"></script>
+		<script src="js/idle-timer.min.js"></script> <!-- http://www.paulirish.com/2009/jquery-idletimer-plugin/ -->
 		
 	</head>
 
